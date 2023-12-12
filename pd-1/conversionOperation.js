5fa39ad450f9f00bad4c3849d4157ddf4a4a0afc
Amit Kumar <crazyone.amit@gmail.com>
2023-12-12 17:21:38 +0530

    small update

--- pd-1/conversionOperation.js
+++ pd-1/conversionOperation.js
@@ -1,9 +1,9 @@
-// let score=33;
-// let score="33";
-// let score="33a";
-// let score=null;
-// let score;
-// let score = undefined;
+// let score=33;-------->Number
+// let score="33";------->String
+// let score="33a";------->String
+// let score=null;--------->Null
+// let score;-------------->Undefined
+// let score = ["Amit"];----->symbol
 let score = true;
 
 let typeValue = Number(score);
