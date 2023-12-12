2d81f3d9d64c421bb691d69b7c97629af5377af2
Amit Kumar <crazyone.amit@gmail.com>
2023-12-12 16:29:28 +0530

    conversion

---
+++ pd-1/conversionOperation.js
@@ -0,0 +1,13 @@
+// let score=33;
+// let score="33";
+// let score="33a";
+// let score=null;
+// let score;
+// let score = undefined;
+let score = true;
+
+let typeValue = Number(score);
+
+console.log(typeof typeValue);
+console.log(typeValue);
+
