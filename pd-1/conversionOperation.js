db3c674ba48e9006353d4746df65bcd5304468f1
Amit Kumar <crazyone.amit@gmail.com>
2023-12-12 19:19:26 +0530

    some extra thing added.

--- pd-1/conversionOperation.js
+++ pd-1/conversionOperation.js
@@ -6,8 +6,6 @@
 // let score = ["Amit"];----->symbol
 //let score = true;      // boolean
 
-
-
 // let typeValue = Number(score);
 
 // console.log(typeof typeValue);
@@ -18,5 +16,24 @@
 // let isLogin=" ";-------------->true
 // let isLogin="";--------------->false
 
-let booleanLogin=Boolean(isLogin);
-console.log(booleanLogin);
+// let booleanLogin=Boolean(isLogin);
+// console.log(booleanLogin);
+
+
+
+
+let num = "16y5t4r33e22e11w9k8l8n7b6";
+
+// Regular expression to match numbers
+let numbers = num.match(/\d+/g);
+
+// Convert matched numbers to integers
+let integers = numbers.map(Number);
+
+console.log(integers);
+
+// console.log(result);
+// let result = parseInt(num.replace(/[^0-9]/g, ""));
+// console.log(result);
+
+// console.log(element);
