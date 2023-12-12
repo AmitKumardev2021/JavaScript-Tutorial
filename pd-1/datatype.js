dbad71ae0fbf5d27297cc26c562cfcd9af6fba89
Amit Kumar <crazyone.amit@gmail.com>
2023-12-12 14:39:03 +0530

    datatypeinfo

---
+++ pd-1/datatype.js
@@ -0,0 +1,19 @@
+//"use strict"//one use update javascript syntax
+
+/*
+let notify=alert(3+1);
+console.log(notify);
+
+above alert will not run because node support outside or browser but alert run inside of browser.
+*/
+
+let giveNumber=1254786985;
+let veryBigNum=9875899956845n;
+let anyName="Amit"
+let boolean = true|false;
+let sky = null;
+let motorcycle;
+let symbol=["goat"];
+
+console.log(typeof symbol);
+console.table([giveNumber,veryBigNum,anyName,boolean,sky,motorcycle])
