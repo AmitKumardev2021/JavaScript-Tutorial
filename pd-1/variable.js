97f1b77a2d2fdbce45a4eeca1aa8bf6fb98e0f8b
Amit Kumar <crazyone.amit@gmail.com>
2023-12-12 14:39:27 +0530

    Variable_info

---
+++ pd-1/variable.js
@@ -0,0 +1,8 @@
+const accountId ='ab124';
+let accountName="Amit@Amit.com"
+var accountPassword='9877654a'
+accountCity="patna"
+let phoneNo;
+
+
+console.table([accountId,accountName,accountPassword,accountCity,phoneNo]);
