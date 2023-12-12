24580de866e3270a93ee06ff20e65140e45f2f05
Amit Kumar <crazyone.amit@gmail.com>
2023-12-12 17:30:04 +0530

    conversion of variable

--- pd-1/conversionOperation.js
+++ pd-1/conversionOperation.js
@@ -4,10 +4,19 @@
 // let score=null;--------->Null
 // let score;-------------->Undefined
 // let score = ["Amit"];----->symbol
-let score = true;
+//let score = true;      // boolean
 
-let typeValue = Number(score);
 
-console.log(typeof typeValue);
-console.log(typeValue);
 
+// let typeValue = Number(score);
+
+// console.log(typeof typeValue);
+// console.log(typeValue);
+
+// let isLogin=1;---------------->true
+// let isLogin="Amit";----------->true
+// let isLogin=" ";-------------->true
+// let isLogin="";--------------->false
+
+let booleanLogin=Boolean(isLogin);
+console.log(booleanLogin);
