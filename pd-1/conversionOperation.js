aae6b91d6375d0ee47b5004009ac8e9176bc9ef7
Amit Kumar <crazyone.amit@gmail.com>
2023-12-14 12:00:24 +0530

    update on 14-dec-2023

--- pd-1/conversionOperation.js
+++ pd-1/conversionOperation.js
@@ -22,18 +22,47 @@
 
 
 
-let num = "16y5t4r33e22e11w9k8l8n7b6";
+// let num = "16y5t4r33e22e11w9k8l8n7b6";
 
 // Regular expression to match numbers
-let numbers = num.match(/\d+/g);
+// let numbers = num.match(/\d+/g);
 
 // Convert matched numbers to integers
-let integers = numbers.map(Number);
+// let integers = numbers.map(Number);
 
-console.log(integers);
+// console.log(integers);
 
 // console.log(result);
 // let result = parseInt(num.replace(/[^0-9]/g, ""));
 // console.log(result);
 
 // console.log(element);
+
+
+
+// ----------------------------------operator
+
+// let value = 3;
+// let NegValue = -value;
+
+// console.log(NegValue);
+
+
+// console.log(2+2);---->add
+// console.log(2-2);----->subtract
+// console.log(2*2);------>multiply
+// console.log(2**3);------>power of number
+// console.log(2/2);------->divide
+// console.log(2%2);-------->reminder
+
+// let str1="Amit"
+// let str2="kumar"
+// let str3=str1+str2;
+
+// console.log(str3);
+
+// console.log("1"+2);
+// let num="1"+2+2
+// console.log(typeof num);
+// console.log("1"+2+2);
+// console.log((3+4)*5%3);
---
+++ pd-1/datatype_summury.js
@@ -0,0 +1,36 @@
+// primitive
+
+// 7-type of data type: String, Number, BigInit, Boolean,Null,Undefined,symbol.
+
+// let score = 256;
+// let scoreValue = 2360.25;
+// const isLoggedIn = true;
+// let result;
+// let choose = null;
+// let id = Symbol("123"); //both symbol value is same but there memory value is different always
+// let LoggedId = Symbol("123");
+// // let bigIntType=1234598765432195989999555647899n;    this is BigInit datatype
+
+// console.log(id === LoggedId); //to Check both symbol with same value but result is false because both memory location is different
+// reference(Non-Primitive)
+
+//array,functions,Object.
+
+// array
+// const arrayOne = ["Amit", "Sumit", "Superman", "IronMan"];
+
+// object
+
+// let teaMaking=Symbol("teaMaking")
+
+// let tea = {
+//   teaName: "kadak chai",
+//   teaPlace: "noida",
+//   [teaMaking]: "put water and tea and boil with sugar",
+// };
+
+// console.log(tea[teaMaking]);
+// // function
+// const myFunction = function () {
+//   console.log("Hello Earth");
+// };
